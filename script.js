b06be1000f427ee40015a7ccd7cc908282655f9d
pbain63 <prodipbain63@gmail.com>
2024-03-25 17:36:13 +0600

    Put the copied JS code from index.html to script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(x, y) {
+  return x + y;
+}
+function subtract(x, y) {
+  return x - y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+function divide(x, y) {
+  return x / y;
+}
+//   console.log(divide(45, 0));
+let firstNumber = x;
+let operator = operatorOption;
+let secondNumber = y;
