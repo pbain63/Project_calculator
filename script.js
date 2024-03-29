061d948261c26b99c3dd6dbd1f31fc29d7b625d1
pbain63 <prodipbain63@gmail.com>
2024-03-30 00:25:35 +0600

    Clean code in script.js

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 const displayContainer = document.querySelector(".display-container");
 const display = document.createElement("div");
+const displayResult = document.createElement("div");
+
 display.classList.add("display");
+displayResult.classList.add("displayResult");
 
 displayContainer.appendChild(display);
-// //-
-// // const seven = document.querySelector("#"+CSS.escape('7'));
-// // const seven = document.querySelector(`#${CSS.escape(7)}`);
+displayContainer.appendChild(displayResult);
 
-// const nine = document.querySelector("[id='9']");
-// const eight = document.querySelector("[id='8']");
-// const seven = document.querySelector("[id='7']");
-// const six = document.querySelector("[id='6']");
-// const five = document.querySelector("[id='5']");
-// const four = document.querySelector("[id='4']");
-// const three = document.querySelector("[id='3']");
-// const two = document.querySelector("[id='2']");
-// const one = document.querySelector("[id='1']");
-// const zero = document.querySelector("[id='0']");
-// const ac = document.querySelector("#ac");
-// const undo = document.querySelector("#undo");
-// // const percent = document.querySelector("[id='%']");
-// const division = document.querySelector("[id='/']");
-// const times = document.querySelector("[id='*']");
-// const minus = document.querySelector("[id='-']");
-// const plus = document.querySelector("[id='+']");
-// const decimal = document.querySelector("[id='.']");
-// const equal = document.querySelector("[id='=']");
+
+
+const calculatorButtons = document.querySelectorAll(".cal-button");
+
+let firstNumber;
+let operator;
+let secondNumber;
+// let num1;
+// let num2;
+
+calculatorButtons.forEach((calButton) => {
+  calButton.addEventListener("click", userSelect);
+});
+
+function userSelect() {
+  // e.target.style.backgroundColor = "orange";
+
+   firstNumber = this.innerHTML;
+  if (
+    firstNumber == "0" ||
+    firstNumber == "1" ||
+    firstNumber == "2" ||
+    firstNumber == "3" ||
+    firstNumber == "4" ||
+    firstNumber == "5" ||
+    firstNumber == "6" ||
+    firstNumber == "7" ||
+    firstNumber == "8" ||
+    firstNumber == "9"
+  ) {
+    display.textContent += firstNumber;
+    console.log(display.textContent);
+    // userChoiceEqual();
+    
+  } else {
+    userChoiceOperator();
+  }
+  
+}
 
 // nine.addEventListener("click", userChoice);
 // eight.addEventListener("click", userChoice);
@@ -45,77 +66,54 @@ displayContainer.appendChild(display);
 
 // equal.addEventListener("click", equalUserChoice);
 
-// let firstNumber;
-// let operator;
-// let secondNumber;
+function userChoiceOperator() {
+  // operator = event.target.id;
+  // let operatorChoice = this.innerHTML;
+  // console.log(operator);
 
-// function userChoice() {
-//   //for button (event)
-//   let firstNumber = this.innerHTML;
-//   // let displayValue = event.target.id;
-//   // event.target.style.backgroundColor = "orange";
-//   // console.log(displayValue);
+  if (firstNumber == "/") {
+    firstNumber = divide;
+  } else if (firstNumber == "*") {
+    firstNumber = multiply;
+  } else if (firstNumber == "-") {
+    firstNumber = subtract;
+  } else if (firstNumber == "+") {
+    firstNumber = add;
+  }
+  display.textContent = "";
+  userChoiceEqual();
 
-//   display.textContent += firstNumber;
-//   // let num2 = num1;
-//   // console.log(num2);
-//   console.log(display.textContent);
+  // userChoice(event);
+  // console.log(userChoice(event));
+}
 
-//   let num1 = firstNumber;
-//   operatorUserChoice();
-//   let result = operate(num1, num2, operator);
-// }
-// // console.log(display.textContent);
+function userChoiceEqual(value) {
+  // let equalButton = this.innerHTML;
+  let num1 = firstNumber;
+  console.log(num1);
+  operator = value;
+  let num2 = secondNumber;
+  let result = operate(num1, num2, operator);
+  displayResult.textContent = result;
+}
 
-// function operatorUserChoice() {
-//   // operator = event.target.id;
-//   let operator = this.innerHTML;
-//   console.log(operator);
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
 
-//   if (
-//     operator == "/" ||
-//     operator == "*" ||
-//     operator == "-" ||
-//     operator == "+"
-//   ) {
-//     return (display.textContent = "");
-//   } else {
-//     return display.textContent;
-//   }
-
-//   // userChoice(event);
-//   // console.log(userChoice(event));
-// }
-
-// function equalUserChoice() {
-//   // let equalButton = this.innerHTML;
-//   display.textContent = operate();
-//   return display.textContent;
-//   // console.log(equalButton);
-// }
-
-// function add(num1, num2) {
-//   return num1 + num2;
-// }
-// function subtract(num1, num2) {
-//   return num1 - num2;
-// }
-// function multiply(num1, num2) {
-//   return num1 * num2;
-// }
-// function divide(num1, num2) {
-//   return num1 / num2;
-// }
-
-// function operate(num1, num2, operator) {
-//   // console.log(num1);
-//   let solution = operator(num1, num2);
-//   // display.textContent = solution;
-//   return solution;
-//   // let solution = displayValue operator displayValue ;
-//   // console.log(solution);
-//   // display.textContent = solution;
-//   userChoice();
-//   operatorUserChoice();
-// }
-// // console.log(operate(8, 6, multiply));
+function operate(num1, num2, operator) {
+  // console.log(num1);
+  return operator(num1, num2);
+  // display.textContent = solution;
+  // return solution;
+}
+// console.log(operate(8, 6, multiply));
