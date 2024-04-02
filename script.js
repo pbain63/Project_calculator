4ebea8b7c0ab51db7da354b4d15fbe5b80d8e306
pbain63 <prodipbain63@gmail.com>
2024-04-02 17:01:56 +0600

    'Modify script.js'
    "Comment out the userChoiceEqual() function to merge
    it with userChoiceEqual()"

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function userSelect() {
     // display.textContent = "";
 
     display.textContent += firstNumber;
-    firstNumberChoice = display.textContent;
+    // firstNumberChoice = display.textContent;
     console.log(display.textContent);
     userChoiceEqual();
   } else if (
@@ -61,7 +61,7 @@ function userSelect() {
 
     console.log(operatorSign);
     userChoiceOperator();
-  } else if (firstNumber == "3" || firstNumber == "4") {
+  } else if ((firstNumber !== "") && (operatorSign !== "")) {
     // display.textContent = "";
 
     display.textContent += firstNumber;
@@ -69,7 +69,7 @@ function userSelect() {
     // console.log(display.textContent);
     console.log(secondNumber);
     userChoiceEqual();
-  }
+  } 
 }
 
 function userChoiceOperator() {
@@ -84,31 +84,31 @@ function userChoiceOperator() {
   } else if (operatorSign == "+") {
     operatorSign = add;
   }
-  display.textContent = "";
+  // display.textContent = "";
   userChoiceEqual();
 }
 
-function userChoiceEqual() {
-  // let equalButton = this.innerHTML;
-  if (secondNumber) {
-    num2 = secondNumber;
-    console.log(num2);
-  } else if (operatorSign) {
-    operator = operatorSign;
-    console.log(operator);
-  } else if (firstNumberChoice) {
-    num1 = firstNumberChoice;
-    console.log(num1);
-  }
-  // let num1 = firstNumber;
-  // console.log(num1);
-  // operator = operatorSign;
-  // console.log(operator);
-  // let num2 = secondNumber;
-  // console.log(num2);
-  // let result = operate(num1, num2, operator);
-  // displayResult.textContent = result;
-}
+// function userChoiceEqual() {
+//   // let equalButton = this.innerHTML;
+//   if (secondNumber) {
+//     num2 = secondNumber;
+//     console.log(num2);
+//   } else if (operatorSign) {
+//     operator = operatorSign;
+//     console.log(operator);
+//   } else if (firstNumber) {
+//     num1 = firstNumber;
+//     console.log(num1);
+//   }
+//   // let num1 = firstNumber;
+//   // console.log(num1);
+//   // operator = operatorSign;
+//   // console.log(operator);
+//   // let num2 = secondNumber;
+//   // console.log(num2);
+//   // let result = operate(num1, num2, operator);
+//   // displayResult.textContent = result;
+// }
 function add(num1, num2) {
   return num1 + num2;
 }
